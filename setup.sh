300e48175a68b25a3111169f48ee5514aceed626
Jef Vlamings <vlamingsjef@gmail.com>
2024-01-10 10:51:32 +0100

    Add .gitignore and adjust tmux conf

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.config/nvim ~/.config/nvim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.ctags ~/.ctags
+ln -s ~/dotfiles/.gitignore ~/.gitignore
 tmux source-file ~/.tmux.conf
